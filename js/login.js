83c09fbc843fc58d75465c7b2e83a94d1af2b757
bestrocker221 <albertoscola221@gmail.com>
2016-12-13 23:41:46 +0100

    basic login page ready

--- js/login.js
+++ js/login.js
@@ -22,21 +22,66 @@ $(function (){
     $('.alert .close').on('click', function(e) {
         //$(this).parent().hide();
         $(this).parent().fadeOut(400);
+        $("#row-user").removeClass("has-error has-feedback");
+        $("#row-password").removeClass("has-error has-feedback");
+        $("#row-name").removeClass("has-error has-feedback");
+        $("#row-surname").removeClass("has-error has-feedback");
+        $("#row-email").removeClass("has-error has-feedback");
+        $("#row-address").removeClass("has-error has-feedback");
+        $("#row-psw").removeClass("has-error has-feedback");
     });
 
+    //LOGIN BUTTON ROUTINE
     $("#login-submit").click(function (event) {
-        console.log("entro");
         if($("#username").val().length < 8){
-            console.log("entro2");
             $("#error-div").fadeIn(150);
-            $("#row-user").addClass("has-error");
-            $("#row-user").addClass("has-feedback");
+            addWarningToElement($("#row-user"));
         } else if($("#password").val().length < 8) {
             $("#error-div").fadeIn(150);
-            $("#row-password").addClass("has-error");
-            $("#row-password").addClass("has-feedback");
+            addWarningToElement($("#row-password"));
         }
         event.preventDefault();
     });
+
+    function addWarningToElement(elem){
+        elem.addClass("has-error has-feedback");
+    }
+
+    function activateAlert(){
+        $("#error-div-register").fadeIn(150);
+    }
+
+    function toggleWarningsOn() {
+        var ok = 0;
+        if($("#name").val().length < 5){
+           ok = 1;
+           addWarningToElement($("#row-name"));
+        }
+        if($("#surname").val().length < 5){
+            ok = 1;
+            addWarningToElement($("#row-surname"));
+        }
+        if($("#email-register").val().length < 5){
+            ok = 1;
+            addWarningToElement($("#row-email"));
+        }
+        if($("#address").val().length < 5){
+            ok = 1;
+            addWarningToElement($("#row-address"));
+        }
+        if($("#password").val().length < 5){
+            ok = 1;
+            addWarningToElement($("#row-psw"));
+        }
+        if(ok!=0){
+            activateAlert();
+        }
+    }
+
+    //REGISTER BUTTON ROUTINE
+    $("#register-submit").on('click', function (e) {
+        toggleWarningsOn();
+        e.preventDefault();
+    })
 
 });
