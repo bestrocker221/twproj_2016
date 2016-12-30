9e1d96e0fc66aa22f6e666f6fff16ea5f3a355df
bestrocker221 <albertoscola221@gmail.com>
2016-12-30 17:03:41 +0100

    fix table css

--- core/cusb/event-manager.php
+++ core/cusb/event-manager.php
@@ -7,42 +7,47 @@
  */
 require '../db_conn.php';
 
-//$id = $_SESSION['member-id'];
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-$id = 6;
+    //$id = $_SESSION['member-id'];
+    $id = 6;
 
-$sqlA = "SELECT * from Events";
-$sqlB = "SELECT ID_EV from follow_Ev WHERE ID_Member='$id'";
+    $sqlA = "SELECT * from Events";
+    $sqlB = "SELECT ID_EV from follow_Ev WHERE ID_Member='$id'";
 
-$tot = array();
+    $tot = array();
 
-$temp2 = array();
-$result = $db->query($sqlB);
-if($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $temp2[$row['ID_EV']]="yes";
-    }
-}
-
-$result = $db->query($sqlA);
-if($result->num_rows > 0){
-    $atot = array();
-    while($row = $result->fetch_assoc()){
-        $temp = array();
-        $temp['id_ev'] = $row['ID_EV'];
-        $temp['description'] = $row['description'];
-        $temp['date'] = $row['date'];
-        $temp['n_partec'] = $row['n_partec'];
-        if(array_key_exists($temp['id_ev'], $temp2)){
-            $temp['iscritto'] = "yes";
-            unset($temp2[$$temp['id_ev']]);
+    $temp2 = array();
+    $result = $db->query($sqlB);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $temp2[$row['ID_EV']] = "yes";
         }
-        array_push($tot, $temp);
     }
-    //$tot['list'] = $atot;
-} else {
-    echo "ERROR " . $db->errno;
-    echo "num row" . $result->num_rows;
+
+    $result = $db->query($sqlA);
+    if ($result->num_rows > 0) {
+        $atot = array();
+        while ($row = $result->fetch_assoc()) {
+            $temp = array();
+            $temp['id_ev'] = $row['ID_EV'];
+            $temp['description'] = $row['description'];
+            $temp['date'] = $row['date'];
+            $temp['n_partec'] = $row['n_partec'];
+            if (array_key_exists($temp['id_ev'], $temp2)) {
+                $temp['iscritto'] = "yes";
+                unset($temp2[$$temp['id_ev']]);
+            }
+            array_push($tot, $temp);
+        }
+        //$tot['list'] = $atot;
+    } else {
+        echo "ERROR " . $db->errno;
+        echo "num row" . $result->num_rows;
+    }
+    $tot = json_encode($tot);
+    echo $tot;
+} else if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    $sql = "INSERT INTO";
 }
-$tot = json_encode($tot);
-echo $tot;
